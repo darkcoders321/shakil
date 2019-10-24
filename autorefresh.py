7c426e8840281849626a21824e36091b71a25950
darkcoders321 <darkcoder321@gmail.com>
2019-10-24 14:33:21 +0600

    autorefresh

---
+++ autorefresh.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+for i in range(5):
+    
+    pyautogui.click(169,102)
+    pyautogui.rightClick (169,102)
+    pyautogui.moveTo(213,157)
+    pyautogui.click(213,157)
+
--- genarel.py
+++ genarel.py
@@ -21,10 +21,16 @@ while run:
     keys=pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
             x-=vel
+    if  keys[pygame.K_LEFT]:
+            x=-vel
     if keys[pygame.K_RIGHT]:
             x+=vel
+    if keys[pygame.K_RIGHT]:
+            x=+vel
     if keys[pygame.K_UP]:
             y-=vel
+    if keys[pygame.K_UP ]:
+            y=+vel
     if keys[pygame.K_DOWN]:
             y+=vel
 
